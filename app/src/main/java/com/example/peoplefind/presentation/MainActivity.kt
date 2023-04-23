03fd3162259fd14415aaeeedbdf6d4d7781e4ddb
xtitaniumx <124804927+xtitaniumx@users.noreply.github.com>
2023-04-24 00:39:06 +0700

    add view for register test

--- app/src/main/java/com/example/peoplefind/presentation/MainActivity.kt
+++ app/src/main/java/com/example/peoplefind/presentation/MainActivity.kt
@@ -1,48 +1,61 @@
 package com.example.peoplefind.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.peoplefind.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.peoplefind.databinding.ActivityMainBinding
 import com.example.peoplefind.domain.extension.onFailure
 import com.example.peoplefind.domain.extension.onLoading
 import com.example.peoplefind.domain.extension.onSuccess
 import com.example.peoplefind.domain.model.Role
+import koleton.api.hideSkeleton
+import koleton.api.loadSkeleton
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
     private val viewModel by viewModel<MainViewModel>()
     private val tokenViewModel by viewModel<TokenViewModel>()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        initView()
+        setContentView(binding.root)
+    }
 
+    private fun initView() = with(binding) {
         val errorHandler = object: BaseViewModel.CoroutinesErrorHandler {
             override fun onError(message: String) {
                 Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
             }
         }
 
-        viewModel.registerAccount(
-            coroutinesErrorHandler = errorHandler,
-            email = "abc@gmail.com",
-            birthDate = "2023-04-23T11:49:12.491Z",
-            password = "abc",
-            passwordConfirm = "abc",
-            role = Role.Admin
-        )
+        buttonRegister.setOnClickListener {
+            viewModel.registerAccount(
+                coroutinesErrorHandler = errorHandler,
+                email = "abc@gmail.com",
+                birthDate = "2023-04-23T11:49:12.491Z",
+                password = "abc",
+                passwordConfirm = "abc",
+                role = Role.Admin
+            )
+        }
 
-        viewModel.authInfo.observe(this) { result ->
+        viewModel.authInfo.observe(this@MainActivity) { result ->
             result.onLoading {
-                Toast.makeText(this, "Loading...", Toast.LENGTH_LONG).show()
+                //Toast.makeText(this@MainActivity, "Loading...", Toast.LENGTH_LONG).show()
+                textAccountInfo.loadSkeleton()
             }.onSuccess {
                 tokenViewModel.saveToken(
                     token = it.accessToken, refreshToken = it.refreshToken
                 )
-                Toast.makeText(this, it.userId, Toast.LENGTH_LONG).show()
+                //Toast.makeText(this@MainActivity, it.userId, Toast.LENGTH_LONG).show()
+                textAccountInfo.text = it.userId
+                textAccountInfo.hideSkeleton()
             }.onFailure { errorMessage, code ->
-                Toast.makeText(this, "Message: $errorMessage, code: $code", Toast.LENGTH_LONG).show()
+                textAccountInfo.text = "Message: $errorMessage, code: $code"
+                textAccountInfo.hideSkeleton()
             }
         }
     }
