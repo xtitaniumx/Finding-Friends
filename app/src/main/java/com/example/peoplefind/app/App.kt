0cfff1a209613909a0c807ee2742293e3334c006
xtitaniumx <124804927+xtitaniumx@users.noreply.github.com>
2023-04-23 23:26:08 +0700

    add skeleton lib

--- app/src/main/java/com/example/peoplefind/app/App.kt
+++ app/src/main/java/com/example/peoplefind/app/App.kt
@@ -1,16 +1,19 @@
 package com.example.peoplefind.app
 
 import android.app.Application
+import com.example.peoplefind.R
 import com.example.peoplefind.di.appModule
 import com.example.peoplefind.di.dataModule
 import com.example.peoplefind.di.domainModule
+import koleton.SkeletonLoader
+import koleton.SkeletonLoaderFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 import timber.log.Timber
 
-class App : Application() {
+class App : Application(), SkeletonLoaderFactory {
     override fun onCreate() {
         super.onCreate()
 
@@ -20,5 +23,12 @@ class App : Application() {
             androidContext(this@App)
             modules(appModule, dataModule, domainModule)
         }
+    }
+
+    override fun newSkeletonLoader(): SkeletonLoader {
+        return SkeletonLoader.Builder(this)
+            .color(R.color.purple_500)
+            .cornerRadius(32F)
+            .build()
     }
 }
